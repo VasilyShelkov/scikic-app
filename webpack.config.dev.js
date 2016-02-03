a918bc1dbd0ec214a0600a4393d6dc4c9779c8cd
Vasily Shelkov <vasilydshelkov@gmail.com>
2016-02-03 18:48:06 +0000

    Using React-Hot-Dev-Starter-Kit as a template for the Rebuild of the Scikic Front End

---
+++ webpack.config.dev.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-hot-middleware/client',
+    './app/App.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'app')
+    }, {
+      test: /\.css$/,
+      loader: 'style!css',
+      include: path.join(__dirname, 'css')
+    }]
+  }
+};
