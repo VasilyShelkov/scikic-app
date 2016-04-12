3a40b8ed598d48296ed424f77b1c24419d886345
Vasily Shelkov <vasilydshelkov@gmail.com>
2016-04-12 23:47:35 +0100

    changed wording and colours of buttons InterestedInitialQuestion

--- src/chat/ChatApp.js
+++ src/chat/ChatApp.js
@@ -4,6 +4,7 @@ import QuestionsList from './QuestionsList';
 const ChatApp = ({ children }) => (
 	<div className="col-xs-12 col-md-6 col-lg-5 col-xl-4">
 		{children}
+		<br />
 		<QuestionsList />
 	</div>
 );
--- src/chat/InterestedInitialQuestion.js
+++ src/chat/InterestedInitialQuestion.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const InterestedInitialQuestion = ({ interested }) => (
   <div className="ui attached message">
-    <div className="header">initial question {interested}</div>
+    <div className="header">Interested ? {interested}</div>
     <br />
     <div className="ui fluid buttons">
-      <button className="ui negative button">Yes!</button>
+      <button className="ui positive button">Yes!</button>
       <div className="or"></div>
-      <button className="ui positive button">Nooooo</button>
+      <button className="ui negative button">Nooooo</button>
     </div>
   </div>
 );
