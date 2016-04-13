8117334e6535d5d34133197be085fc21e105932d
Vasily Shelkov <vasilydshelkov@gmail.com>
2016-04-13 21:59:35 +0100

    Got the initial cycle working for interested

--- src/chat/InterestedInitialQuestion.js
+++ src/chat/InterestedInitialQuestion.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Typist from 'react-typist';
 
-const InterestedInitialQuestion = ({ interested, onClick }) => {
+const InterestedInitialQuestion = ({ onClick }) => {
   let optionButtons;
   return (
     <div className="ui attached message">
       <Typist className="header" startDelay={1000}
         onTypingDone={() => $(optionButtons).transition('swing down')}
-        cursor={{ hideWhenDone: !interested }}
       >
         Interested ? &nbsp;
       </Typist>
@@ -15,20 +14,8 @@ const InterestedInitialQuestion = ({ interested, onClick }) => {
       <div className="ui fluid buttons transition hidden" ref={(node) => optionButtons = node}>
         <button className="ui positive button" onClick={() => onClick(true)}>Yes!</button>
         <div className="or"></div>
-        <button className="ui negative button" onClick={() => {
-          $(optionButtons).transition('swing down');
-          $('.Typist.header').css('display', 'none');
-          onClick(false);
-        }}>
-          Nooooo
-        </button>
+        <button className="ui negative button" onClick={() => onClick(false)}>Nooooo</button>
       </div>
-      {interested === false && (
-        <Typist className="header" startDelay={1000}>
-          It is a shame you do not want to try me . . . &nbsp; <br />
-          Thank you for your interest !
-        </Typist>
-      )}
     </div>
   );
 };
--- src/chat/QuestionsList.js
+++ src/chat/QuestionsList.js
@@ -1,34 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Typist from 'react-typist';
 import { isUserInterested } from './chatActions';
 import InterestedInitialQuestion from './InterestedInitialQuestion';
 
 
 class QuestionsList extends Component {
 
-  componentDidMount() {
-    console.log('did mount');
-  }
-
-  componentWillUpdate() {
-    console.log('will update');
+  constructor(props) {
+    super(props);
+    this.initial = null;
+    this.questions = null;
+    this.uninterested = null;
   }
 
   componentDidUpdate() {
-    console.log('did update');
+    if (this.props.chat.interested === false) {
+      $(this.initial).transition({
+        animation: 'slide down',
+        onComplete: () => $(this.uninterested).transition('slide down')
+      });
+    }
+
+    if (this.props.chat.interested) {
+      $(this.initial).transition({
+        animation: 'slide down',
+        onComplete: () => $(this.questions).transition('slide down')
+      });
+    }
   }
 
   render() {
-    console.log('render');
     const { chat, onUserInterested } = this.props;
     return (
       <div>
-        {
-          chat.interested ?
-          'questions'
-          :
-            <InterestedInitialQuestion interested={chat.interested} onClick={onUserInterested} />
-        }
+        <div className="ui segment transition hidden" ref={(node) => this.questions = node }>
+          questions
+        </div>
+
+        <div ref={(node) => this.initial = node }>
+          <InterestedInitialQuestion interested={chat.interested} onClick={onUserInterested} />
+        </div>
+
+        <div className="transition hidden" style={{ width: '100%' }} ref={(node) => this.uninterested = node }>
+          <div className="ui message">
+            {chat.interested === false && (
+              <Typist className="header" startDelay={1000}>
+                It is a shame you do not want to try me . . . &nbsp; <br />
+                Thank you for your interest !
+              </Typist>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
@@ -36,7 +59,7 @@ class QuestionsList extends Component {
 QuestionsList.propTypes = {
   chat: React.PropTypes.object,
   onUserInterested: React.PropTypes.func,
-}
+};
 
 const mapStateToProps = (state) => ({ chat: state.chat });
 
